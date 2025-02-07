af2da37b2a8dbb40b1f4ef656425720075284e65
MichalKalke <michalkalke@gmail.com>
2025-02-07 12:14:55 +0100

    add tmp

--- components/buildless-serverless/internal/controller/resources/deployment.go
+++ components/buildless-serverless/internal/controller/resources/deployment.go
@@ -187,6 +187,11 @@ func (d *Deployment) volumes() []corev1.Volume {
 			VolumeSource: corev1.VolumeSource{
 				EmptyDir: &corev1.EmptyDirVolumeSource{},
 			},
+		}, corev1.Volume{
+			Name: "tmp",
+			VolumeSource: corev1.VolumeSource{
+				EmptyDir: &corev1.EmptyDirVolumeSource{},
+			},
 		})
 	}
 	return volumes
@@ -224,6 +229,11 @@ func (d *Deployment) volumeMounts() []corev1.VolumeMount {
 				ReadOnly:  true,
 				MountPath: path.Join(d.workingSourcesDir(), "package-registry-config/pip.conf"),
 				SubPath:   "pip.conf",
+			},
+			corev1.VolumeMount{
+				Name:      "tmp",
+				ReadOnly:  false,
+				MountPath: "/tmp",
 			})
 	}
 	return volumeMounts
